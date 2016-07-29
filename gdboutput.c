2fc750707911a58ada0cd800e42db76545e000bb
bhaskar4n <baskar4n@gmail.com>
2016-07-30 02:07:33 +0530

    Create gdboutput.c

---
+++ gdboutput.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int add(int a 0x7fffffffdccc , int b 0x7fffffffdcc8)
+{
+  int output; 0x7fffffffdcdc
+  output = a+b;
+  return output;
+}
+
+int main()
+{
+    int answer; 0x7fffffffdcfc
+    answer = add(3,5);
+  return 0;
+}
